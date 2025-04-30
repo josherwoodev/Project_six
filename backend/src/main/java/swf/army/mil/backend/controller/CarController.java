0a3a52855dd2004ce9eddd2584c4816bbc28e0eb
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-30 14:10:38 -0500

    car controller get all buildout

--- backend/src/main/java/swf/army/mil/backend/controller/CarController.java
+++ backend/src/main/java/swf/army/mil/backend/controller/CarController.java
@@ -1,17 +1,30 @@
 package swf.army.mil.backend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import swf.army.mil.backend.entity.CarEntity;
+import swf.army.mil.backend.service.CarService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/api")
 @CrossOrigin
 public class CarController {
+    private final CarService carService;
 
-    @GetMapping
-    public String test() {
-        return "[\"Hello World\"]";
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
+
+    @GetMapping("/cars")
+    public ResponseEntity<List<CarEntity>> getAllCars()
+    {
+        return ResponseEntity.ok(carService.getAllCars());
+    }
+
 }
--- backend/src/test/java/swf/army/mil/backend/controller/CarControllerTest.java
+++ backend/src/test/java/swf/army/mil/backend/controller/CarControllerTest.java
@@ -1,21 +1,28 @@
 package swf.army.mil.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import swf.army.mil.backend.entity.CarEntity;
+import swf.army.mil.backend.service.CarService;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CarController.class)
 public class CarControllerTest {
 
     public static final String HELLO_WORLD = "Hello World";
-    @Autowired
-    private MockMvc mvc;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -23,16 +30,27 @@ public class CarControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void shouldReceiveHelloWorld() throws Exception {
-        mockMvc.perform(get("/test"))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$.length()").value(1))
-                .andExpect(jsonPath("$[0]").value(HELLO_WORLD));
+    @MockitoBean
+    CarService carService;
+
+    private CarEntity car1;
+    private  CarEntity car2;
+    private List<CarEntity> cars;
+
+    @BeforeEach
+    void setUp() {
+        car1=  new CarEntity("Ford", "Mustang", 2000, 1222.22, true);
+        car2= new CarEntity("Toyota", "Camry", 1955, 20.21, false);
+        cars = List.of(car1, car2);
+
+        when(carService.getAllCars()).thenReturn(cars);
     }
 
     @Test
     void shouldReturnListOfCars() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/cars"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/cars"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+        verify(carService,times(1)).getAllCars();
     }
 }
