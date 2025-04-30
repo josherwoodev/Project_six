5d8d42047d894569c2c568fdfbf8f98d131bbac4
Joshua Smith <joshua.m.smith292.mil@army.mil>
2025-04-30 13:21:03 -0500

    add entity

---
+++ backend/src/main/java/swf/army/mil/backend/entity/CarEntity.java
@@ -0,0 +1,80 @@
+package swf.army.mil.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CarEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+    private String model;
+    private Integer year;
+    private Double price;
+    private Boolean isUsed;
+
+
+    public CarEntity() {
+    }
+
+    public CarEntity(String make, String model, Integer year, Double price, Boolean isUsed) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.isUsed = isUsed;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(Boolean used) {
+        isUsed = used;
+    }
+}
